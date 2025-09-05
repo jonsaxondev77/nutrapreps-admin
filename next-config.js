b6f17b96d98868eb6165cfbe4cd392554e87b37f
jonsaxondev77 <saxonjonathan6@gmail.com>
2025-09-05 16:33:13 +0100

    Updated workflow

---
+++ next-config.js
@@ -0,0 +1,48 @@
+// next.config.js
+const nextConfig = {
+  /* config options here */
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+        port: '',
+        pathname: '/150/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      }, 
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**'
+      }, 
+      {
+        protocol: 'https',
+        hostname: 'nutrapreps.b-cdn.net', 
+        port: '',
+        pathname: '/**',
+      }
+    ]
+  }
+};
+
+module.exports = nextConfig;
