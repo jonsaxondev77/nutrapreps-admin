5b5763654b6ccb6da5cbf22a20415b4e8e20bf86
jonsaxondev77 <saxonjonathan6@gmail.com>
2025-09-09 16:29:50 +0100

    Updated paging on Orders

--- src/app/admin/orders/OrdersTable.tsx
+++ src/app/admin/orders/OrdersTable.tsx
@@ -92,6 +92,11 @@ const OrdersTable = () => {
                         <option value={25}>25</option>
                         <option value={50}>50</option>
                         <option value={100}>100</option>
+                        <option value={250}>250</option>
+                        <option value={500}>500</option>
+                        <option value={1000}>1000</option>
+                        <option value={1500}>1500</option>
+                        <option value={2000}>2000</option>
                     </select>
                 </div>
             </div>
